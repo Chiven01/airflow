560f4c1418a98d7dc18908a1d0951c17184c3b7b
huangruiyan <huangruiyan@sogou-inc.com>
2019-11-27 14:35:06 +0800

    added import

--- airflow/utils/m2crypto.py
+++ airflow/utils/m2crypto.py
@@ -6,6 +6,7 @@ import base64
 from urllib.parse import unquote
 import M2Crypto
 
+from airflow import settings
 
 ptoken = """BypZbX86xpNv32EUZr1uk8V%2Fw4OjhpN9i54WgfgiL%2BJ9K0RCXvIVks8Jwork6e7jbtUapH7WI%2BaXhfNLyXC6qgfjYPsgMNtwG7xut3SQ8No6%2B64XvjmioR6y%2BOXc%2FKmQkVPDQHjELAeUCoKQHnm9Trq%2FhVeG0dnBC5HZSKJl%2Fjbz4vglNo%2FF4Y0%2BaII5gRaGyMaA1FLxrrvzrr%2F2B7YssIFPnvhs5XexVFwm%2FdrIeY5yyZQLSbBcikAjXIINwRDsCqEDQZhIukqoQx%2F%2FgxkHsYiqemlpT8kDwwVj9FVnq6X8iNVPH72q0MG9Gpr%2BH61Gk7sG6bKNE%2F8uX1XgG9gqIQ%3D%3D"""
 
